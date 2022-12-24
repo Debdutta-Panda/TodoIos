01804570baa6278f011896921b695ab62ce2cd68
Debdutta Panda <me@debduttapanda.com>
2022-12-24 19:49:37 +0530

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Debdutta Panda on 24/12/22.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
